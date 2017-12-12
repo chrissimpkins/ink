ef4c43e9e8fa425999b4a068e3ebed1a3d2c29c0
Chris Simpkins <git.simpkins@gmail.com>
2017-12-11 21:48:16 -0500

    added new standard input stream validator to validators package

---
+++ validators/stream.go
@@ -0,0 +1,43 @@
+// stream holds standard stream validators for the ink application
+/*
+MIT License
+
+Copyright (c) 2017 Chris Simpkins
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package validators
+
+import "os"
+
+// test os.Stdin for presence of data, if present return true, else return false
+func StdinValidates(stdin *os.File) bool {
+	f, err := stdin.Stat()
+	if err != nil { // unable to obtain file data with Stat() method call = fail
+		return false
+	}
+
+	size := f.Size()
+	if size == 0 { // there does not appear to be any data in the stdin stream = fail
+		return false
+	}
+
+	return true
+}
