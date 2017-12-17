d33a37c5064775465bd17c7a039ba22bdfcda6d3
Chris Simpkins <git.simpkins@gmail.com>
2017-12-17 00:50:32 -0500

    added validators/lint.go source tests

---
+++ validators/lint_test.go
@@ -0,0 +1,27 @@
+package validators
+
+import "testing"
+
+func TestLintTemplateSuccessValidTemplate(t *testing.T) {
+	result, _ := LintTemplateSuccess("../testfiles/template_1.txt.in")
+	if result == false {
+		t.Errorf("[FAIL] LintTemplateSuccess returned false for a valid template, expected true.")
+	}
+}
+
+func TestLintTemplateSuccessInvalidTemplate(t *testing.T) {
+	result, _ := LintTemplateSuccess("../testfiles/template_2.txt.in")
+	if result == true {
+		t.Errorf("[FAIL] LintTemplateSuccess returned true for an invalid template, expected false.")
+	}
+}
+
+func TestLintTemplateSuccessBadFilePath(t *testing.T) {
+	result, err := LintTemplateSuccess("../testfiles/bogusfile.txt")
+	if result == true {
+		t.Errorf("[FAIL] LintTemplateSuccess returned true for a file path to a missing file, expected false.")
+	}
+	if err == nil {
+		t.Errorf("[FAIL] LintTemplateSuccess returned nil for error when a file path to a missing file was tested, expected error message.")
+	}
+}
