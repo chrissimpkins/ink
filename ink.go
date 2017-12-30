89d84f127313d8613de0ec6a3b716b00bded4056
Chris Simpkins <git.simpkins@gmail.com>
2017-12-30 16:42:02 -0500

    version bump

--- ink.go
+++ ink.go
@@ -42,7 +42,7 @@ import (
 
 const (
 	// Version is the application version string
-	Version = "0.7.1"
+	Version = "0.7.2"
 
 	// Usage is the application usage string
 	Usage = `Usage: ink [options] [template path 1]...[template path n]
